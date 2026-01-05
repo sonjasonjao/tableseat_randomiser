a00eada6f7472963d73751d2bb706cce9ae50921
Sonja Ojala <sonjaojala@Sonja-MacBook-Air.local>
2026-01-05 21:58:58 +0200

    Refactoring: function calls now from .js with event listeners, only one submit button and name mode choice as radio buttons, etc

--- seats.js
+++ seats.js
@@ -1,39 +1,60 @@
-function addSingleNameField(namesDiv) {
-	let newInput = document.createElement('input');
-	newInput.type = 'text';
-	newInput.className = 'names';
-	newInput.id = 'name' + (namesDiv.children.length - 1);
-	namesDiv.appendChild(newInput);
+const	table = document.querySelector("#result-table");
+const	nameFields = document.querySelector(".names");
+
+const	nameForm = document.querySelector(".name-mode-input");
+nameForm.addEventListener("change", () => {
+	const	nameMode = document.querySelector('input[name="name-mode"]:checked').value;
+	if (nameMode == "manual")
+		addNameFields();
+	if (nameMode == "default")
+		clearNameFields();
+});
+
+const	runButton = document.querySelector("#run");
+runButton.addEventListener("click", () => {
+	if (document.querySelector('input[name="name-mode"]:checked') != null)
+		run();
+	else {
+		table.innerHTML = "";
+		let msg = document.createElement('p');
+		msg.innerText = "Please give names or choose to use running numbers!";
+		msg.className = "error";
+		table.appendChild(msg);
+	}
+});
+
+function addSingleNameField(nameFields) {
+	let newInput = document.createElement("input");
+	newInput.type = "text";
+	newInput.className = "names";
+	newInput.id = "name" + (nameFields.children.length - 1);
+	nameFields.appendChild(newInput);
+}
+
+function clearNameFields() {
+	nameFields.innerHTML = "";
+	table.innerHTML = "";
 }
 
 function addNameFields() {
-	let mode = document.querySelector('input[name=\"name-mode"]:checked').value;
-	let namesDiv = document.querySelector(".names");
-	let table = document.querySelector("#result-table");
-	if (mode == "default") {
-		namesDiv.innerHTML = "";
-		table.innerHTML = "";
-		document.querySelector('.shape-input').style.display = "flex";
-		document.querySelector('.run').style.display = "flex";
-		return ;
-	}
 	const amount = document.querySelector("#people-amt").value;
-	namesDiv.innerHTML = "";
+	nameFields.innerHTML = "";
+	table.innerHTML = "";
 	if (amount <= 0 || amount > 30) {
+		document.querySelector(".shape-input").style.display = "none";
+		document.querySelector("#run").style.display = "none";
 		let newInput = document.createElement('p');
 		newInput.innerText = "Number of people must be between 1 and 30!";
 		newInput.className = "error";
-		namesDiv.appendChild(newInput);
-	}	
-	table.innerHTML = "";
+		nameFields.appendChild(newInput);
+		return ;
+	}
 	let infoText = document.createElement('p');
-	infoText.className = 'names-info';
+	infoText.className = "names-info";
 	infoText.textContent = "Write the names below:";
-	namesDiv.appendChild(infoText);
+	nameFields.appendChild(infoText);
 	for (let i = 0; i < amount; i++)
-		addSingleNameField(namesDiv);
-	document.querySelector('.shape-input').style.display = "flex";
-	document.querySelector('.run').style.display = "flex";
+		addSingleNameField(nameFields);
 }
 
 function getNamedPeople(amount) {
@@ -51,10 +72,9 @@ function getDefaultPeople(amount) {
 }
 
 function run() {
-	let mode = document.querySelector('input[name=\"name-mode"]:checked').value;
+	let mode = document.querySelector('input[name="name-mode"]:checked').value;
 	const amount = document.querySelector("#people-amt").value;
 	if (amount <= 0 || amount > 30) {
-		let table = document.querySelector("#result-table");
 		table.innerHTML = "";
 		let msg = document.createElement('p');
 		msg.innerText = "Number of people must be between 1 and 30!";
@@ -84,7 +104,6 @@ function makeTable(people) {
 	const shape = document.querySelector("#shape-choice").value;
 	let width;
 	const height = 2;
-	let table = document.querySelector("#result-table");
 	table.innerHTML = "";
 	table.style.maxHeight = (100 * height) + "px";
 	if (shape == "rectangle" || (shape == "square" && people.length % 2)) {
