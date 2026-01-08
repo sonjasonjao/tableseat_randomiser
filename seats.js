bc3ab709b08da0f8a9315f73e4926511d3e1b257
Sonja Ojala <sojala@c2r1p3.hive.fi>
2026-01-08 20:29:54 +0200

    Horizontal scrollability for resulting table, no need for people amount limit. 0 or negative error handling works. Still need to test overlapping of elements, and now names are not cut but chairs adjusted. Whole page layout missing.

--- seats.js
+++ seats.js
@@ -1,12 +1,29 @@
 const	table = document.querySelector("#result-table");
+const	realTable = document.querySelector(".scroll-container");
 const	nameFields = document.querySelector(".names");
 
 const	nameForm = document.querySelector(".name-mode-input");
 nameForm.addEventListener("change", () => {
 	const	nameMode = document.querySelector('input[name="name-mode"]:checked').value;
 	clearNameFields();
-	if (nameMode == "manual")
+	const	amount = document.querySelector("#people-amt").value;
+	if (amount <= 0) {
+		document.querySelector(".shape-input").style.display = "none";
+		document.querySelector("#run").style.display = "none";
+		let	newInput = document.createElement('p');
+		newInput.innerText = "Number of people must be between 1 and 30!";
+		newInput.className = "error";
+		nameFields.appendChild(newInput);
+	}
+	else if (nameMode == "manual") {
+		document.querySelector(".shape-input").style.display = "flex";
+		document.querySelector("#run").style.display = "flex";
 		addNameFields();
+	}
+	else if (nameMode == "default") {
+		document.querySelector(".shape-input").style.display = "flex";
+		document.querySelector("#run").style.display = "flex";
+	}
 });
 
 const	runButton = document.querySelector("#run");
@@ -14,16 +31,16 @@ runButton.addEventListener("click", () => {
 	if (document.querySelector('input[name="name-mode"]:checked') != null)
 		run();
 	else {
-		table.innerHTML = "";
-		let msg = document.createElement('p');
+		realTable.innerHTML = "";
+		let	msg = document.createElement('p');
 		msg.innerText = "Please give names or choose to use running numbers!";
 		msg.className = "error";
-		table.appendChild(msg);
+		realTable.appendChild(msg);
 	}
 });
 
 function addSingleNameField(nameFields) {
-	let newInput = document.createElement("input");
+	let	newInput = document.createElement("input");
 	newInput.type = "text";
 	// newInput.maxLength = "40";
 	newInput.className = "names";
@@ -33,21 +50,12 @@ function addSingleNameField(nameFields) {
 
 function clearNameFields() {
 	nameFields.innerHTML = "";
-	table.innerHTML = "";
+	realTable.innerHTML = "";
 }
 
 function addNameFields() {
-	const amount = document.querySelector("#people-amt").value;
-	if (amount <= 0) { // || amount > 30) {	// the disappearing and reappearing of run element is not working!
-		document.querySelector(".shape-input").style.display = "none";
-		document.querySelector("#run").style.display = "none";
-		let newInput = document.createElement('p');
-		newInput.innerText = "Number of people must be between 1 and 30!";
-		newInput.className = "error";
-		nameFields.appendChild(newInput);
-		return ;
-	}
-	let infoText = document.createElement('p');
+	const	amount = document.querySelector("#people-amt").value;
+	let		infoText = document.createElement('p');
 	infoText.className = "names-info";
 	infoText.textContent = "Write the names below:";
 	nameFields.appendChild(infoText);
@@ -56,31 +64,31 @@ function addNameFields() {
 }
 
 function getNamedPeople(amount) {
-	let people = [];
+	let	people = [];
 	for (let i = 0; i < amount; i++)
 		people.push(document.querySelector("#name" + i).value);
 	return people;
 }
 
 function getDefaultPeople(amount) {
-	let people = [];
+	let	people = [];
 	for (let i = 0; i < amount; i++)
 		people.push(i + 1);
 	return people;
 }
 
 function run() {
-	let nameMode = document.querySelector('input[name="name-mode"]:checked').value;
-	const amount = document.querySelector("#people-amt").value;
-	if (amount <= 0) { // || amount > 30) {
-		table.innerHTML = "";
-		let msg = document.createElement('p');
+	let		nameMode = document.querySelector('input[name="name-mode"]:checked').value;
+	const	amount = document.querySelector("#people-amt").value;
+	if (amount <= 0) {
+		realTable.innerHTML = "";
+		let	msg = document.createElement('p');
 		msg.innerText = "Number of people must be between 1 and 30!";
 		msg.className = "error";
-		table.appendChild(msg);
+		realTable.appendChild(msg);
 		return ;
 	}
-	let people = [];
+	let	people = [];
 	if (nameMode == "default")
 		people = getDefaultPeople(amount);
 	else
@@ -90,8 +98,8 @@ function run() {
 
 function randomise(people) {
 	for (let i = people.length - 1; i > 0; i--) {
-		let j = Math.floor(Math.random() * (i + 1));
-		let tmp = people[i];
+		let	j = Math.floor(Math.random() * (i + 1));
+		let	tmp = people[i];
 		people[i] = people[j];
 		people[j] = tmp;
 	}
@@ -99,14 +107,14 @@ function randomise(people) {
 }
 
 function getMaxLength() {
-	let tableStyle = window.getComputedStyle(table);
-	let fontSize = parseFloat(tableStyle.fontSize);
-	let maxSize = parseFloat(tableStyle.width) - 2;
+	let	tableStyle = window.getComputedStyle(table);
+	let	fontSize = parseFloat(tableStyle.fontSize);
+	let	maxSize = parseFloat(tableStyle.width) - 2;
 	return maxSize / fontSize;
 }
 
 function getMaxNameLength(people) {
-	let longest = 0;
+	let	longest = 0;
 	for (i = 0; i < people.length; i++)
 	{
 		if (people[i].length > longest)
@@ -120,9 +128,7 @@ function makeTable(people) {
 	const	pageContent = window.getComputedStyle(document.querySelector(".content"));
 	const	chairSize = getMaxNameLength(people) * (parseFloat(pageContent.fontSize));
 	let		rowWidth, maxWidth;
-	const	height = 2;
-	table.innerHTML = "";
-	table.style.maxHeight = (100 * height) + "px";
+	realTable.innerHTML = "";
 	if (tableShape == "rectangle" || (tableShape == "square" && people.length % 2)) {
 		if (people.length % 2) {	// odd number of people will always have an edge chair on the right side
 			rowWidth = Math.floor(people.length / 2) + 1;
@@ -146,7 +152,7 @@ function makeTable(people) {
 				chair.className = "top-chair chair";
 			else
 				chair.className = "bottom-chair chair";
-			table.appendChild(chair);
+			realTable.appendChild(chair);
 		}
 	}
 	else if (tableShape == "square") {	// even number of people at a round / square table
@@ -167,7 +173,7 @@ function makeTable(people) {
 				chair.className = "top-chair chair";
 			else
 				chair.className = "bottom-chair chair";
-			table.appendChild(chair);
+			realTable.appendChild(chair);
 		}
 	}
 }
